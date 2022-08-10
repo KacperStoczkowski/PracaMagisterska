f6d3ec4d6387310cfa8eba8f5a8a72ea94675e44
Kacper Stoczkowski <kacper.stoczkowski@gmail.com>
2022-08-10 13:48:44 +0200

    feat: Changed input and output image sizes

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,18 @@
 import React from "react";
 import './Menu.css';
 
+const createLookUpArray = (value) => [...Array(256).keys()].map((_, index) => {
+    const val = index + value;
+
+    if (val > 255) {
+        return Math.min(val, 255);
+    } else if (val < 0) {
+        return Math.max(val, 0);
+    } else {
+        return val;
+    }
+});
+
 export const Menu = ({ items = [], onSelect, value }) => {
     return (
         <select name="effect-selector" onChange={onSelect} value={value}>
--- src/services/effects/sepia/sepia.js
+++ src/services/effects/sepia/sepia.js
@@ -1,34 +1,22 @@
-export const sepia = (inputImageCtx, inputImageCanvas) => {
- 
-    // read the width and height of the canvas
-    var width = canvas.width;
-    var height = canvas.height;
-    
-    // convert to sepia
-    var newImageData = ctx.createImageData(width, height);
+import {transparentToWhite} from "../../transparentToWhite";
 
-    for (var i=0; i<height; i++) {
-        for (var j=0; j<width; j++) {
-            index = (i*width+j)*4;
+export const sepia = (inputImageCanvas, sepiaValue) => {
+    const width = inputImageCanvas.width;
+    const height = inputImageCanvas.height;
+    const context = inputImageCanvas.getContext('2d');
+    const imageData = transparentToWhite(context, width, height);
+    const resultImageData = context.createImageData(imageData);
 
-            val = 0.299*imageData.data[index+0] + 0.587*imageData.data[index+1] + 0.114*imageData.data[index+2];
+    for (let i = 0; i < imageData.data.length; i += 4) {
+        const greyScaleValue = 0.299 * imageData.data[i] + 0.587*imageData.data[i + 1] + 0.114 * imageData.data[i + 2];
+        const factorW = greyScaleValue + sepiaValue;
+        const doubledFactorW = greyScaleValue + (2 * sepiaValue) ;
 
-            if (val + 2 * sepiaValue > 255)
-                newImageData.data[index+0] = 255;
-            else
-                newImageData.data[index+0] = val + 2 * sepiaValue;             
-
-            if (val + sepiaValue > 255)
-                newImageData.data[index+1] = 255;
-            else
-                newImageData.data[index+1] = val + sepiaValue;             
-            
-            // Jeśli nie działa spróbować z oryginalnymi nawiasami
-
-            newImageData.data[index+2] = val;
-            newImageData.data[index+3] = 255;
-        }
+        resultImageData.data[i] = doubledFactorW > 255 ? Math.min(doubledFactorW, 255) : doubledFactorW;
+        resultImageData.data[i + 1] = factorW > 255 ? Math.min(factorW, 255) : factorW;
+        resultImageData.data[i + 2] = greyScaleValue;
+        resultImageData.data[i + 3] = 255;
     }
-    
-    return newImageData;
+
+    return resultImageData;
 }
